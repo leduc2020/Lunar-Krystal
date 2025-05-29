6c15a64909faaabce3e62829c977b8319d452e8a
Leduc2010 <leduc2010nb@gmail.com>
2025-05-29 19:26:26 +0700

    Update bím.js

--- modules/commands/bím.js
+++ modules/commands/bím.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-  name: "a",
+  name: "bím",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "hi",
   description: "Random Ảnh 18+",
   commandCategory: "Ảnh",
-  usages: "a",
+  usages: "bím",
   cooldowns: 2,
   dependencies: {
     "request":"",
